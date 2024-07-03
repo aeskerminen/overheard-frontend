890d9e03c16fd1fe7ce4173c6c04ff18b37c4b20
aeskerm <kerminenartturi@gmail.com>
2024-07-03 13:42:38 +0300

    creating a layout..

--- src/App.jsx
+++ src/App.jsx
@@ -22,14 +22,14 @@ const Post = (props) => {
     return <div>Loading...</div>
   else {
     return (
-      <div className="p-2 bg-white shadow-lg rounded self-center flex flex-row gap-4" style={{ minWidth: '5rem', maxWidth: '35rem', maxHeight: '15rem' }}>
+      <div className="mr-2 ml-2 p-2 bg-gray-300 shadow-lg self-center rounded flex flex-row gap-4" style={{ minWidth: '5rem', maxWidth: '35rem', maxHeight: '15rem' }}>
         <div className="flex flex-col justify-between p-2 gap-2">
           <div className="flex items-center gap-2">
             <p className="p-1 bg-slate-800 text-white rounded-xl text-sm">@{post.channel}</p>
             <p>{post.location}</p>
             <p>{post.createdAt}</p>
           </div>
-          <p className="text-md" style={{width: 'calc(100%)', textOverflow: 'ellipsis', overflow: 'hidden'}}>{post.content}</p>
+          <p className="text-md" style={{ width: 'calc(100%)', textOverflow: 'ellipsis', overflow: 'hidden' }}>{post.content}</p>
           <MdModeComment className="self-start m-0" size={30}></MdModeComment>
         </div>
         <div className="flex flex-col items-center mr-2">
@@ -50,12 +50,21 @@ const App = () => {
   const [posts, setPosts] = useState([1, 2, 3, 4, 5, 6])
 
   return (
-    <div className="bg-white w-full h-full flex justify-center ">
-      <div className=" bg-slate-600 p-4 flex flex-col flex-nowrap gap-4 overflow-y-scroll">
-        {posts.map((p, i) =>
-          <Post key={i} post={testPost}></Post>
-        )}
-      </div>
+    <div className="bg-white h-full flex flex-col justify-center ">
+        <div className="flex flex-row justify-center items-center p-2 bg-gray-400">
+          <h1>Helsinki</h1>
+        </div>
+        <div className="flex flex-col  flex-nowrap gap-2 overflow-y-scroll">
+          <div className="bg-gray-400 p-2 flex gap-2 justify-center">
+            <button className="p-2 bg-gray-500 rounded-full text-gray-200 border-2 border-orange-400">Newest</button>
+            <button className="p-2 bg-gray-500 rounded-full text-gray-200 border-2 border-orange-400">Most commented</button>
+            <button className="p-2 bg-gray-500 rounded-full text-gray-200 border-2 border-orange-400">Most liked</button>
+          </div>
+          
+          {posts.map((p, i) =>
+            <Post key={i} post={testPost}></Post>
+          )}
+        </div>
     </div>
   )
 }
