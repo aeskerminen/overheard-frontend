2d042b1677a4734b42bd8aee252074c62b63b850
aeskerm <kerminenartturi@gmail.com>
2024-07-04 12:36:57 +0300

    Abstract components away from main

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,11 @@
 import { useEffect, useState } from "react"
-
-import { MdModeComment } from "react-icons/md";
-import { FaAngleDown } from "react-icons/fa";
-import { FaAngleUp } from "react-icons/fa";
+import Post from "./components/Post";
 
 const testPost = {
   location: 'here',
   channel: 'main',
   content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel dapibus dui. Donec sed turpis eu nulla pulvinar pulvinar. Donec in tortor tempus, egestas diam eget, consequat dui. Cras elit libero, euismod id malesuada in, tempus sed justo. Aenean ut interdum massa. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
   createdAt: '1h',
-}
-
-const Post = (props) => {
-  const [post, setPost] = useState(undefined)
-
-  useEffect(() => {
-    setPost(props.post)
-  }, [props.post])
-
-  if (post === undefined)
-    return <div>Loading...</div>
-  else {
-    return (
-      <div className="mr-2 ml-2 p-2 bg-gray-300 shadow-lg self-center rounded flex flex-row gap-4" style={{ minWidth: '5rem', maxWidth: '35rem', maxHeight: '15rem' }}>
-        <div className="flex flex-col justify-between p-2 gap-2">
-          <div className="flex items-center gap-2">
-            <p className="p-1 bg-slate-800 text-white rounded-xl text-sm">@{post.channel}</p>
-            <p>{post.location}</p>
-            <p>{post.createdAt}</p>
-          </div>
-          <p className="text-md" style={{ width: 'calc(100%)', textOverflow: 'ellipsis', overflow: 'hidden' }}>{post.content}</p>
-          <MdModeComment className="self-start m-0" size={30}></MdModeComment>
-        </div>
-        <div className="flex flex-col items-center mr-2">
-          <p className="text-2xl">...</p>
-          <div className="text-2xl mt-auto mb-auto flex flex-col items-center">
-            <button><FaAngleUp></FaAngleUp></button>
-            <p className="text-center">0</p>
-            <button><FaAngleDown></FaAngleDown></button>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
 }
 
 const App = () => {
---
+++ src/components/Post.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+import { FaAngleDown, FaAngleUp } from "react-icons/fa"
+import { MdModeComment } from "react-icons/md"
+
+const Post = (props) => {
+    const [post, setPost] = useState(undefined)
+
+    useEffect(() => {
+        setPost(props.post)
+    }, [props.post])
+
+    if (post === undefined)
+        return <div>Loading...</div>
+    else {
+        return (
+            <div className="mr-2 ml-2 p-2 bg-gray-300 shadow-lg self-center rounded flex flex-row gap-4" style={{ minWidth: '5rem', maxWidth: '35rem', maxHeight: '15rem' }}>
+                <div className="flex flex-col justify-between p-2 gap-2">
+                    <div className="flex items-center gap-2">
+                        <p className="p-1 bg-slate-800 text-white rounded-xl text-sm">@{post.channel}</p>
+                        <p>{post.location}</p>
+                        <p>{post.createdAt}</p>
+                    </div>
+                    <p className="text-md" style={{ width: 'calc(100%)', textOverflow: 'ellipsis', overflow: 'hidden' }}>{post.content}</p>
+                    <MdModeComment className="self-start m-0" size={30}></MdModeComment>
+                </div>
+                <div className="flex flex-col items-center mr-2">
+                    <p className="text-2xl">...</p>
+                    <div className="text-2xl mt-auto mb-auto flex flex-col items-center">
+                        <button><FaAngleUp></FaAngleUp></button>
+                        <p className="text-center">0</p>
+                        <button><FaAngleDown></FaAngleDown></button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Post
