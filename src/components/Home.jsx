90edf131ab7218081c61ac3f9ed2f95eca285196
aeskerm <kerminenartturi@gmail.com>
2024-07-04 13:25:29 +0300

    Sticking to this easy implementation for a while...

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Post from "./Post";
 
 const testPost = {
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,26 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
+import Login from './components/Login.jsx'
+import Home from './components/Home.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Login />
+    },
+    {
+        path: "/*",
+        children: [
+            {
+                path: "home",
+                element: <Home />
+            },
+        ]
+    }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <RouterProvider router={router}></RouterProvider>
 )
