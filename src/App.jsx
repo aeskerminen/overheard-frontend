64d8bea448c792fddd160feab3324424c24eca27
aeskerm <kerminenartturi@gmail.com>
2024-07-04 13:00:14 +0300

    Add routing and private rout handling (thanks to Makanju Oluwafemi)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import { useEffect, useState } from "react"
 import Home from "./components/Home";
+import { Route, Router, Routes } from "react-router-dom";
+import AuthProvider from "./hooks/AuthProvider";
+import Login from "./components/Login";
+import PrivateRoute from "./hooks/PrivateRoute";
 
 const App = () => {
-  
+  <Router>
+    <AuthProvider>
+      <Routes>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route element={<PrivateRoute></PrivateRoute>}>
+          <Route path="/home" element={Home}></Route>
+        </Route>
+      </Routes>
+    </AuthProvider>
+  </Router>
 }
 
 export default App
