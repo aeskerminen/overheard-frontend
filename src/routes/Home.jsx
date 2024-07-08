7734327e2b4cb92e1492acc4b125439b86d40ec1
aeskerm <kerminenartturi@gmail.com>
2024-07-08 21:47:43 +0300

    create layout for bottom nav

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,6 +3,12 @@ import Post from "../components/Post";
 
 import { IoMdClose } from "react-icons/io";
 
+import { TiHome } from "react-icons/ti";
+import { FaSearch } from "react-icons/fa";
+import { GoBellFill } from "react-icons/go";
+import { IoPerson } from "react-icons/io5";
+
+
 import { createPost, getPosts } from "../services/serviceHandler";
 
 const Home = () => {
@@ -10,12 +16,15 @@ const Home = () => {
     const [location, setLocation] = useState("Locating...");
 
     const [creationModal, setCreationModal] = useState(false)
-    
+
     const [content, setContent] = useState("")
     const [channel, setChannel] = useState("")
     const [color, setColor] = useState("#000000")
 
     const [filter, setFilter] = useState("newest")
+
+    // home, channels, inbox, me
+    const [tab, setTab] = useState("home")
 
     useEffect(() => {
         getPosts().then(result => {
@@ -35,15 +44,15 @@ const Home = () => {
     }
 
     return (
-        <div style={{backgroundColor: '#151515'}} className="h-full w-full flex flex-col justify-center ">
-            <div className="flex flex-row justify-center items-center p-2 text-white" style={{backgroundColor: '#3D3D3D'}}>
+        <div style={{ backgroundColor: '#151515' }} className="h-full w-full flex flex-col justify-center ">
+            <div className="flex flex-row justify-center items-center p-2 text-white" style={{ backgroundColor: '#3D3D3D' }}>
                 <h1>{location}</h1>
             </div>
             <div className="flex flex-col grow flex-nowrap gap-2 pb-2 overflow-y-scroll">
-                <div style={{backgroundColor: '#3D3D3D'}} className=" p-2 flex gap-2 justify-center">
-                    <button onClick={() => setFilter("newest")} style={{backgroundColor: '#5F5F5F', color: '#9F9F9F', borderColor: '#FF9703'}} className={`p-3 font-bold rounded-full text-gray-200 ${filter === 'newest' ? "border-2" : "border-none"}`}>Newest</button>
-                    <button onClick={() => setFilter("mcommented")} style={{backgroundColor: '#5F5F5F', color: '#9F9F9F', borderColor: '#FF9703'}} className={`p-3 font-bold rounded-full text-gray-200 ${filter === 'mcommented' ? "border-2" : "border-none"}`}>Most commented</button>
-                    <button onClick={() => setFilter("mliked")} style={{backgroundColor: '#5F5F5F', color: '#9F9F9F', borderColor: '#FF9703'}} className={`p-3 font-bold rounded-full text-gray-200 ${filter === 'mliked' ? "border-2" : "border-none"}`}>Most liked</button>
+                <div style={{ backgroundColor: '#3D3D3D' }} className=" p-2 flex gap-2 justify-center">
+                    <button onClick={() => setFilter("newest")} style={{ backgroundColor: '#5F5F5F', color: '#9F9F9F', borderColor: '#FF9703' }} className={`p-3 font-bold rounded-full text-gray-200 ${filter === 'newest' ? "border-2" : "border-none"}`}>Newest</button>
+                    <button onClick={() => setFilter("mcommented")} style={{ backgroundColor: '#5F5F5F', color: '#9F9F9F', borderColor: '#FF9703' }} className={`p-3 font-bold rounded-full text-gray-200 ${filter === 'mcommented' ? "border-2" : "border-none"}`}>Most commented</button>
+                    <button onClick={() => setFilter("mliked")} style={{ backgroundColor: '#5F5F5F', color: '#9F9F9F', borderColor: '#FF9703' }} className={`p-3 font-bold rounded-full text-gray-200 ${filter === 'mliked' ? "border-2" : "border-none"}`}>Most liked</button>
                 </div>
 
                 {posts.map((p, i) =>
@@ -54,29 +63,58 @@ const Home = () => {
                         color: p.color,
                         createdAt: p.createdAt,
                         identifier: p.identifier
-                      }}></Post>
+                    }}></Post>
                 )}
             </div>
             {creationModal &&
-                <div style={{zIndex: 999, backdropFilter: 'blur(3px)'}} className="absolute rounded w-full h-full flex justify-center items-center">
-                        <div className="bg-white rounded shadow-lg flex flex-col p-4">
-                            <button onClick={() => setCreationModal(false)} className="p-2 rounded-full bg-gray-100 hover:bg-gray-200 active:bg-gray-300 self-start"><IoMdClose size={25}></IoMdClose></button>
-                            <form className="flex gap-2 mt-4" onSubmit={handleCreatePost}>
-                                <input placeholder="What have you heard...?" className="p-2 text-2xl bg-gray-100 rounded-md" type="text" name="content" onChange={e => setContent(e.target.value)} value={content}></input>
-                                <input placeholder="Choose a channel..." className="p-2 text-2xl bg-gray-100 rounded-md" type="text" name="channel" onChange={e => setChannel(e.target.value)} value={channel}></input>
-                                <div className="p-2 bg-gray-100 rounded-md flex justify-center items-center">
-                                    <input type="button" onClick={() => setColor('#000000')} className="p-6 rounded-full bg-black border-4 border-white"></input>
-                                    <input type="button" onClick={() => setColor('#F97316')} className="p-6 rounded-full bg-orange-500 border-4 border-white"></input>
-                                    <input type="button" onClick={() => setColor('#22C55E')} className="p-6 rounded-full bg-green-500 border-4 border-white"></input>
-                                    <input type="button" onClick={() => setColor('#A855F7')} className="p-6 rounded-full bg-purple-500 border-4 border-white"></input>
-                                </div>
-                                <input className="p-2 text-lg bg-gray-100 hover:bg-gray-200 active:bg-gray-300 self-center rounded-sm" type="submit" name="submit" value="Post..."></input>
-                            </form>
-                        </div>
+                <div style={{ zIndex: 999, backdropFilter: 'blur(3px)' }} className="absolute rounded w-full h-full flex justify-center items-center">
+                    <div className="bg-white rounded shadow-lg flex flex-col p-4">
+                        <button onClick={() => setCreationModal(false)} className="p-2 rounded-full bg-gray-100 hover:bg-gray-200 active:bg-gray-300 self-start"><IoMdClose size={25}></IoMdClose></button>
+                        <form className="flex gap-2 mt-4" onSubmit={handleCreatePost}>
+                            <input placeholder="What have you heard...?" className="p-2 text-2xl bg-gray-100 rounded-md" type="text" name="content" onChange={e => setContent(e.target.value)} value={content}></input>
+                            <input placeholder="Choose a channel..." className="p-2 text-2xl bg-gray-100 rounded-md" type="text" name="channel" onChange={e => setChannel(e.target.value)} value={channel}></input>
+                            <div className="p-2 bg-gray-100 rounded-md flex justify-center items-center">
+                                <input type="button" onClick={() => setColor('#000000')} className="p-6 rounded-full bg-black border-4 border-white"></input>
+                                <input type="button" onClick={() => setColor('#F97316')} className="p-6 rounded-full bg-orange-500 border-4 border-white"></input>
+                                <input type="button" onClick={() => setColor('#22C55E')} className="p-6 rounded-full bg-green-500 border-4 border-white"></input>
+                                <input type="button" onClick={() => setColor('#A855F7')} className="p-6 rounded-full bg-purple-500 border-4 border-white"></input>
+                            </div>
+                            <input className="p-2 text-lg bg-gray-100 hover:bg-gray-200 active:bg-gray-300 self-center rounded-sm" type="submit" name="submit" value="Post..."></input>
+                        </form>
+                    </div>
                 </div>
             }
-            <div className="absolute p-2 flex justify-center items-center w-full" style={{ top: '90%' }}>
-                <button onClick={() => setCreationModal(true)} style={{backdropFilter: 'brightness(50%)'}} className="p-3 border-4 rounded-full aspect-square flex items-center text-2xl text-white">+</button>
+            <div className="absolute p-2 flex justify-center items-center w-full" style={{ top: '85%' }}>
+                <button onClick={() => setCreationModal(true)} style={{ backdropFilter: 'brightness(50%)' }} className="p-3 border-4 rounded-full aspect-square flex items-center text-2xl text-white absolute">+</button>
+            </div>
+
+            <div className="flex flex-row justify-center items-center p-2 text-white" style={{ backgroundColor: '#3D3D3D' }}>
+                <div className="flex justify-center items-center gap-4">
+                    <div className="p-2 text-lg flex flex-col justify-center items-center gap-1 text-gray-300 hover:text-gray-50">
+                        <div className="pl-4 pr-4 pt-1 pb-1 rounded-full flex justify-center items-center" style={tab === "home" ? { backgroundColor: 'orangered' } : {}} onClick={() => setTab("home")}>
+                            <TiHome size={30}></TiHome>
+                        </div>
+                        <p>Home</p>
+                    </div>
+                    <div className="p-2 text-lg flex flex-col justify-center items-center gap-1 text-gray-300 hover:text-gray-50" >
+                        <div className="pl-4 pr-4 pt-1 pb-1 rounded-full flex justify-center items-center" style={tab === "channels" ? { backgroundColor: 'orangered' } : {}} onClick={() => setTab("channels")}>
+                            <FaSearch size={30}></FaSearch>
+                        </div>
+                        <p>Channels</p>
+                    </div>
+                    <div className="p-2 text-lg flex flex-col justify-center items-center gap-1 text-gray-300 hover:text-gray-50" >
+                        <div className="pl-4 pr-4 pt-1 pb-1 rounded-full flex justify-center items-center" style={tab === "inbox" ? { backgroundColor: 'orangered' } : {}} onClick={() => setTab("inbox")}>
+                            <GoBellFill size={30}></GoBellFill>
+                        </div>
+                        <p>Inbox</p>
+                    </div>
+                    <div className="p-2 text-lg flex flex-col justify-center items-center gap-1 text-gray-300 hover:text-gray-50" >
+                        <div className="pl-4 pr-4 pt-1 pb-1 rounded-full flex justify-center items-center" style={tab === "me" ? { backgroundColor: 'orangered' } : {}} onClick={() => setTab("me")}>
+                            <IoPerson size={30}></IoPerson>
+                        </div>
+                        <p>Me</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
