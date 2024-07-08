f74d93b8771cd0944fcfa9473e6fda43b33b3822
aeskerm <kerminenartturi@gmail.com>
2024-07-08 22:20:45 +0300

    fix css for better responsivity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <div className=" flex flex-row justify-center items-center p-2 text-white" style={{ backgroundColor: '#3D3D3D' }}>
+        <div className="flex flex-row justify-center items-center p-2 text-white" style={{ backgroundColor: '#3D3D3D' }}>
             <h1>{"location"}</h1>
         </div>
     )
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
     }
 
     return (
-        <div style={{ backgroundColor: '#151515'}} className="overflow-y-scroll">
+        <div style={{ backgroundColor: '#151515'}} className="grow overflow-y-scroll">
             <div className="flex flex-col grow flex-nowrap gap-2 mb-2 overflow-y-scroll">
                 <div style={{ backgroundColor: '#3D3D3D' }} className="p-2 flex gap-2 justify-center">
                     <button onClick={() => setFilter("newest")} style={{ backgroundColor: '#5F5F5F', color: '#9F9F9F', borderColor: '#FF9703' }} className={`p-3 font-bold rounded-full text-gray-200 ${filter === 'newest' ? "border-2" : "border-none"}`}>Newest</button>
