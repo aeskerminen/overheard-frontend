df922d44194a482cc5fac244078b99227bb043a3
aeskerm <kerminenartturi@gmail.com>
2024-07-29 17:05:54 +0300

    stylize login screen

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -34,17 +34,17 @@ const Login = () => {
     }
 
     return (
-        <div className="bg-white h-full w-full flex flex-col justify-center items-center">
-            <div className="flex flex-col justify-center gap-2 bg-slate-300 p-4 rounded">
-                <h1 className="self-center text-2xl">Overheard</h1>
-                <form className="p-2 bg-white flex gap-2" onSubmit={handleLogin} encType="multipart(form-data" method="POST">
-                    <input className="p-2 text-lg" type="email" placeholder="Add your email..." name="email" onChange={e => setCurUsername(e.target.value)}></input>
-                    <input className="p-2 text-lg" type="password" placeholder="Enter your password..." name="password" onChange={e => setCurPassword(e.target.value)}></input>
+        <div style={{backgroundColor: "#151515"}} className=" h-full w-full flex flex-col justify-center items-center">
+            <div style={{backgroundColor: "#3D3D3D"}} className="flex flex-col justify-center gap-2 p-4 rounded">
+                <h1 className="self-center text-4xl m-4 text-white">OVERHEARD</h1>
+                <form style={{backgroundColor: "#5F5F5F"}} className="p-2 flex gap-2" onSubmit={handleLogin} encType="multipart(form-data" method="POST">
+                    <input className="p-2 text-lg border-0 bg-transparent border-b-2" type="email" placeholder="Enter your email..." name="email" onChange={e => setCurUsername(e.target.value)}></input>
+                    <input className="p-2 text-lg border-0 bg-transparent border-b-2" type="password" placeholder="Enter your password..." name="password" onChange={e => setCurPassword(e.target.value)}></input>
                     <input className="rounded grow p-2 text-lg hover:bg-gray-100 active:bg-gray-200" type="submit" value="Login"></input>
                 </form>
-                <form className="p-2 bg-white flex gap-2" onSubmit={handleRegister} encType="multipart(form-data" method="POST">
-                    <input className="p-2 text-lg" type="email" placeholder="Add your email..." name="email" onChange={e => setCurUsername(e.target.value)}></input>
-                    <input className="p-2 text-lg" type="password" placeholder="Enter your password..." name="password" onChange={e => setCurPassword(e.target.value)}></input>
+                <form style={{backgroundColor: "#5F5F5F"}} className="p-2 flex gap-2" onSubmit={handleRegister} encType="multipart(form-data" method="POST">
+                    <input className="p-2 text-lg border-0 bg-transparent border-b-2" type="email" placeholder="Enter your email..." name="email" onChange={e => setCurUsername(e.target.value)}></input>
+                    <input className="p-2 text-lg border-0 bg-transparent border-b-2" type="password" placeholder="Enter your password..." name="password" onChange={e => setCurPassword(e.target.value)}></input>
                     <input className="rounded grow p-2 text-lg hover:bg-gray-100 active:bg-gray-200" type="submit" value="Register"></input>
                 </form>
             </div>
