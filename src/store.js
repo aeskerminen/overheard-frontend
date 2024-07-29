9c58820660c5a100e42213b6844b847ab968d9e6
aeskerm <kerminenartturi@gmail.com>
2024-07-29 12:41:12 +0300

    add reducer to store.js

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import creationModalReducer from "./reducers/creationModalSlice";
 import postsReducer from "./reducers/postsSlice";
+import locationReducer from "./reducers/locationSlice";
 
 export const store = configureStore({
   reducer: {
     modal: creationModalReducer,
     posts: postsReducer,
+    location: locationReducer,
   },
 });
